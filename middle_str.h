a7df5b77c352e81859225ff392c73d6fcc5c4489
Michael <77501487+Medvezar@users.noreply.github.com>
2021-11-08 12:25:35 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -3,3 +3,8 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+bool itc_isDigit(unsigned char ch1); // 1
+unsigned char itc_toUpper(unsigned char ch1); // 2
+unsigned char itc_register(unsigned char ch1); // 3
+bool itc_compare(string str1, string str2); // 4
