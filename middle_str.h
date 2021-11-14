6005750caec79be50a6bc11dc656322fe0bbc149
Michael <77501487+Medvezar@users.noreply.github.com>
2021-11-14 16:24:24 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -4,8 +4,10 @@
 #include <string>
 using namespace std;
 
+long long itc_len(string str1); // test func
 bool itc_isDigit(unsigned char ch1); // 1
 unsigned char itc_toUpper(unsigned char ch1); // 2
-unsigned char itc_register(unsigned char ch1); // 3
+unsigned char itc_changeCase(unsigned char ch1); // 3
 bool itc_compare(string str1, string str2); // 4
-int itc_kol_slov(string str); // 5
+int itc_countWords(string str); // 5
+string itc_maxCharWord(string str); // 6
