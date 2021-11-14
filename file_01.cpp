391a1d6e8dec204723b892cb7eb6e990ed974296
Michael <77501487+Medvezar@users.noreply.github.com>
2021-11-14 23:09:24 +0300

    Update file_01.cpp

--- file_01.cpp
+++ file_01.cpp
@@ -1,21 +1,21 @@
 #include "middle_str.h"
 
 
-bool itc_isDigit(unsigned char ch1){ // 1
+bool itc_isDigit(unsigned char ch1) { // 1
     if (ch1 >= 48 && ch1 <= 57)
         return true;
     return false;
 }
 
 
-unsigned char itc_toUpper(unsigned char ch1){ // 2
+unsigned char itc_toUpper(unsigned char ch1) { // 2
     if (ch1 >= 97 && ch1 <= 122)
         return (ch1 - 32);
     return ch1;
 }
 
 
-unsigned char itc_register(unsigned char ch1){ // 3
+unsigned char itc_changeCase(unsigned char ch1) { // 3
     if (ch1 >= 97 && ch1 <= 122)
         return itc_toUpper(ch1);
     else if (ch1 >= 65 && ch1 <= 91)
@@ -25,9 +25,9 @@ unsigned char itc_register(unsigned char ch1){ // 3
 }
 
 
-bool itc_compare(string str1, string str2){ // 4
+bool itc_compare(string str1, string str2) { // 4
     int times = 0;
-    while (str1[times] != '\0' || str2[times] != '\0'){
+    while (str1[times] != '\0' || str2[times] != '\0') {
         if (str1[times] != str2[times])
             return false;
         times++;
@@ -36,12 +36,12 @@ bool itc_compare(string str1, string str2){ // 4
 }
 
 
-int itc_kol_slov(string str){
+int itc_countWords(string str) { // 5
     int numTime = 0;
     int times = 0;
     bool test = false;
-    while (str[times] != '\0'){
-        if (test == false && str[times] != ' '){
+    while (str[times] != '\0') {
+        if (test == false && str[times] != ' ' && (str[times] >= 97 && str[times] <= 122)){
             test = true;
             numTime++;
         }
@@ -50,4 +50,3 @@ int itc_kol_slov(string str){
         times++;
     }
     return numTime;
-}
