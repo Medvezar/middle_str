30e68e8c5b8fedc7f3b2e699fe976c2f61f68007
Michael <77501487+Medvezar@users.noreply.github.com>
2021-11-16 22:02:41 +0300

    Update file_02.cpp

--- file_02.cpp
+++ file_02.cpp
@@ -40,3 +40,78 @@ char itc_sameChar(string str) { // 7
         times++;
     }
 }
+
+bool itc_isFirstInSecond(string str2, string str1) { // 8
+    bool test = true;
+    char text1 = 'a';
+    char text2 = 'a';
+    for (int n1 = 0; n1 < itc_len(str1); n1++) {
+        if (str1[n1] == str2[0]) {
+            test = true;
+            if ((itc_len(str1) - (n1 + 1)) >= itc_len(str2) - 1) {
+                for (int n2 = 1; n2 < itc_len(str2); n2++) {
+                    text1 = str1[n1 + n2];
+                    text2 = str2[n2];
+                    if (text1 != text2)
+                        test = false;
+                }
+                if (test == true)
+                    return true;
+            }
+        }
+    }
+    return false;
+}
+
+string itc_Cezar(string str, int changeNum) {
+    string textRes = "";
+    int times = 0;
+    int change;
+    while (str[times] != '\0') {
+        change = changeNum;
+        if (str[times] >= 65 && str[times] <= 90) {
+            if (str[times] + change > 90) {
+                change = changeNum % 26;
+                textRes += 64 + change;
+            }
+            else if (str[times] + change < 65) {
+                change = changeNum % 26;
+                textRes += change + 91;
+            }
+            else
+                textRes += str[times] + change;
+        }
+        else if (str[times] >= 97 && str[times] <= 122) {
+            if (str[times] + change > 122) {
+                change = changeNum % 26;
+                textRes += 96 + change;
+            }
+            else if (str[times] + change < 97) {
+                change = changeNum % 26;
+                textRes += 123 + change;
+            }
+            else
+                textRes += str[times] + change;
+        }
+        times++;
+    }
+    return textRes;
+}
+
+string itc_rmFreeSpace(string str) {
+    int times = 0;
+    bool test = false;
+    string textRes = "";
+    while (str[times] != '\0') {
+        if (test == false && str[times] == ' ') {
+            textRes += str[times];
+            test = true;
+        }
+        else if (str[times] != ' ') {
+            test = false;
+            textRes += str[times];
+        }
+        times++;
+    }
+    return textRes;
+}
