5c013f06e681196bf34fa9a46f586f7447c105c2
Michael <77501487+Medvezar@users.noreply.github.com>
2021-11-17 20:50:05 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -8,6 +8,7 @@ long long itc_len(string str1); // test func
 long long abs1(long long num); // test func
 string to_strInt(int num); // test func
 int to_intStr(string text); // test func
+string to_bin(string text); // test func
 bool itc_isDigit(unsigned char ch1); // 1
 unsigned char itc_toUpper(unsigned char ch1); // 2
 unsigned char itc_changeCase(unsigned char ch1); // 3
@@ -19,3 +20,4 @@ bool itc_isFirstInSecond(string str2, string str1); // 8
 string itc_Cezar(string str, int change); // 9
 string itc_rmFreeSpace(string str); // 10
 bool itc_isIp(string str); // 11
+string itc_DecToBin(string str); // 12
